95d4ce0cf7c76eb62d6171fb1c2348f5e0aa63f2
Kalil Peixoto <kalilmvp@gmail.com>
2018-07-20 13:46:47 +0100

    getting user input

--- blockchain.py
+++ blockchain.py
@@ -8,8 +8,12 @@ def get_last_blockchain_value():
 def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
-add_value(5.3)
-add_value(10, get_last_blockchain_value())
-add_value(9.5, get_last_blockchain_value())
+
+def get_user_input():
+    return float(input('Your transaction amount: '))
+
+add_value(get_user_input())
+add_value(get_user_input(), get_last_blockchain_value())
+add_value(get_user_input(), get_last_blockchain_value())
 
 print(blockchain)
