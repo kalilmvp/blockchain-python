54cfe74f223f29f65a45c69c8314b26015dc7f1f
Kalil Peixoto <kalilmvp@gmail.com>
2018-08-08 10:28:40 +0100

    Inheritance to print the object

--- block.py
+++ block.py
@@ -1,14 +1,13 @@
 from time import time
 
-class Block:
+from printable import Printable
+
+class Block(Printable):
     
     def __init__(self, index, previous_hash, transactions, proof, timestamp=None):
         self.index = index
         self.previous_hash = previous_hash
         self.transactions = transactions
         self.proof = proof
-        self.time = time() if timestamp is None else timestamp
-
-    def __repr__(self):
-        return 'index: {} / previous_hash: {} / transactions: {} / proof: {} / timestamp: {}'.format(self.index, self.previous_hash, self.transactions, self.proof, self.time)
+        self.time = time() if timestamp is None else timestamp 
         
